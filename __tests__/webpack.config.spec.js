b464eb27436603db63d2bbfc3b3ffdd39fa66146
Shane Garrity <shanedgarrity@gmail.com>
2019-08-17 00:13:56 -0400

    Improve config snapshot readability
    Prettify config snapshot json to improve readability and/of diffs.

--- __tests__/webpack.config.spec.js
+++ __tests__/webpack.config.spec.js
@@ -8,14 +8,14 @@ describe('webpack config', () => {
 
   test('matches development snapshot', () => {
     const env = {};
-    expect(JSON.stringify(webpackConfig(env))).toMatchSnapshot();
+    expect(JSON.stringify(webpackConfig(env), null, 4)).toMatchSnapshot();
   });
 
   test('matches production snapshot', () => {
     const env = {
       production: true,
     };
-    expect(JSON.stringify(webpackConfig(env))).toMatchSnapshot();
+    expect(JSON.stringify(webpackConfig(env), null, 4)).toMatchSnapshot();
   });
 
 });
