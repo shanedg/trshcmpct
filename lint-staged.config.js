777c35aa0ced6576753f0ed7dcd9c8046df0ee85
Shane Garrity <shanedgarrity@gmail.com>
2019-09-17 21:45:33 -0400

    improvement: ignore config files from linting
    Avoid rare but weird issues with linting tools running aginst their own
    configuration files.
    Ignore eslint config from being linted by eslint. Ignore lint-staged
    config from being picked up by itself.
    Remove negated eslint ignore pattern flag from package lint script, as
    well as from lint-staged command.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  '*.js': [
-    'eslint --ignore-pattern \'!.eslintrc.js\' --max-warnings=0 --fix',
+  // ignore .eslintrc.js and lint-staged.config.js
+  '!(.eslintrc|lint-staged.config)*.js': [
+    'eslint --max-warnings=0 --fix',
     'git add',
   ],
 };
