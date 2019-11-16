903ac4dd8b757d46cafc114eb7f373050fa2b940
Shane Garrity <shanedgarrity@gmail.com>
2019-11-16 12:51:51 -0500

    chore: remove ignored ESLint CLI option
    ESLint doesn't respect --ext option when passed files directly; instead,
    files are linted regardless of extension so we can omit it.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // ignore .eslintrc.js and lint-staged.config.js and .huskyrc.js
   '!(.eslintrc|lint-staged.config|.huskyrc)*.{js,jsx,ts,tsx}': [
-    'eslint --ext .js,.jsx,.ts,.tsx --max-warnings=0 --fix --cache',
+    'eslint --max-warnings=0 --fix --cache',
     'git add',
   ],
 };
