7098efdbe798c7d15fa73535dceb39e2bf287bb9
Shane Garrity <shanedgarrity@gmail.com>
2019-11-17 05:38:08 -0500

    test: overhaul Webpack config unit tests

--- config/__tests__/webpack.config.spec.js
+++ config/__tests__/webpack.config.spec.js
@@ -1,53 +1,103 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const serializer = require('jest-serializer-path');
+const webpack = require('webpack');
 
 const webpackConfig = require('../../webpack.config');
 
 // Remove absolute paths from snapshots.
 expect.addSnapshotSerializer(serializer);
 
-describe('webpack config', () => {
-  const development = {};
-  const production = { production: true };
+describe('webpack', () => {
+  const developmentEnvironment = {};
+  const productionEnvironment = { production: true };
+  let developmentConfig;
+  let productionConfig;
 
-  it('matches development snapshot', () => {
-    const env = {};
-    expect(webpackConfig(env)).toMatchSnapshot();
+  describe('common config', () => {
+
+    beforeAll(() => {
+      developmentConfig = webpackConfig(developmentEnvironment);
+      productionConfig = webpackConfig(productionEnvironment);
+      Object.freeze(developmentConfig);
+      Object.freeze(productionConfig);
+    });
+
+    it('sets entry points', () => {
+      expect(developmentConfig.entry).toHaveProperty('index', './src/index.ts');
+      expect(productionConfig.entry).toStrictEqual(developmentConfig.entry);
+    });
+
+    it('sets output', () => {
+      expect(developmentConfig.output).toHaveProperty('filename', '[name].[chunkhash].js');
+      expect(developmentConfig.output).toHaveProperty('path', expect.any(String));
+      expect(productionConfig.output).toStrictEqual(developmentConfig.output);
+    });
+
+    it('sets module rules', () => {
+      expect(developmentConfig.module).toHaveProperty('rules', expect.any(Array));
+      expect(productionConfig.module).toStrictEqual(developmentConfig.module);
+    });
+
+    it('sets optimizations', () => {
+      expect(developmentConfig.optimization).toHaveProperty('splitChunks', expect.any(Object));
+      expect(productionConfig.optimization).toStrictEqual(developmentConfig.optimization);
+    });
+
+    it('sets plugins', () => {
+      expect(developmentConfig.plugins).toMatchObject([
+        expect.any(CleanWebpackPlugin),
+        expect.any(webpack.ProgressPlugin),
+        expect.any(HtmlWebpackPlugin),
+      ]);
+      // Can't use deep-equality checks around plugins since they're unique class instances so repeat same assertion against productionConfig.
+      // Otherwise, toEqual/toStrictEqual comparisons fail with "Received: serializes to the same string".
+      expect(productionConfig.plugins).toMatchObject([
+        expect.any(CleanWebpackPlugin),
+        expect.any(webpack.ProgressPlugin),
+        expect.any(HtmlWebpackPlugin),
+      ]);
+    });
+
+    it('sets dev server options', () => {
+      expect(developmentConfig.devServer).toHaveProperty('open', true);
+      expect(productionConfig.devServer).toStrictEqual(developmentConfig.devServer);
+    });
+
   });
 
-  it('matches production snapshot', () => {
-    const env = {
-      production: true,
-    };
-    expect(webpackConfig(env)).toMatchSnapshot();
+  describe('development config', () => {
+
+    beforeAll(() => {
+      developmentConfig = webpackConfig(developmentEnvironment);
+      Object.freeze(developmentConfig);
+    });
+
+    it('matches snapshot', () => {
+      expect(developmentConfig).toMatchSnapshot();
+    });
+
+    it('sets development mode', () => {
+      expect(developmentConfig.mode).toEqual('development');
+    });
+
   });
 
-  // ported from webpack-helpers.spec.js
-  it('sets dev server options', () => {
-    expect(webpackConfig(development).devServer).toStrictEqual({open: true});
-  });
+  describe('production config', () => {
 
-  it('sets entry points', () => {
-    const entries = webpackConfig(development).entry;
-    expect(entries).toEqual(
-      expect.objectContaining({
-        index: expect.any(String),
-      })
-    );
-    const entryList = Object.keys(entries);
-    expect(entryList)
-      .toEqual(
-        expect.arrayContaining(['index'])
-      );
-  });
+    beforeAll(() => {
+      productionConfig = webpackConfig(productionEnvironment);
+      Object.freeze(productionConfig);
+    });
 
-  it('sets development mode by default', () => {
-    expect(webpackConfig(development).mode)
-      .toEqual('development');
-  });
+    it('matches snapshot', () => {
+      expect(productionConfig).toMatchSnapshot();
+    });
 
-  it('sets production mode from environment flag', () => {
-    expect(webpackConfig(production).mode)
-      .toEqual('production');
+    it('sets production mode', () => {
+      expect(productionConfig.mode).toEqual('production');
+    });
+
   });
 
 });
