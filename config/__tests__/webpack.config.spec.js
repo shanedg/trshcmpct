6eccc531225f9b9d42f8af9a1b94bea550797496
Shane Garrity <shanedgarrity@gmail.com>
2019-11-17 02:13:51 -0500

    test: don't stringify Webpack config for snapshot

--- config/__tests__/webpack.config.spec.js
+++ config/__tests__/webpack.config.spec.js
@@ -9,14 +9,14 @@ describe('webpack config', () => {
 
   it('matches development snapshot', () => {
     const env = {};
-    expect(JSON.stringify(webpackConfig(env), null, 4)).toMatchSnapshot();
+    expect(webpackConfig(env)).toMatchSnapshot();
   });
 
   it('matches production snapshot', () => {
     const env = {
       production: true,
     };
-    expect(JSON.stringify(webpackConfig(env), null, 4)).toMatchSnapshot();
+    expect(webpackConfig(env)).toMatchSnapshot();
   });
 
 });
