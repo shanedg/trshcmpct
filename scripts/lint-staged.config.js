b51138a391b0f5ba0db09ac56c11a2564f8c6faa
Shane Garrity <shanedgarrity@gmail.com>
2020-01-27 22:22:12 -0500

    chore: bump lint-staged to v10.0.3

--- scripts/lint-staged.config.js
+++ scripts/lint-staged.config.js
@@ -2,6 +2,5 @@ module.exports = {
   // ignore .eslintrc.js and lint-staged.config.js and .huskyrc.js
   '!(.eslintrc|lint-staged.config|.huskyrc)*.{js,jsx,ts,tsx}': [
     'eslint --max-warnings=0 --fix --cache',
-    'git add',
   ],
 };
