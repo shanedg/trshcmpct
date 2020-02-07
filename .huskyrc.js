fb2917a269d53722f97029c5f9467677bc6293ca
Shane Garrity <shanedgarrity@gmail.com>
2020-02-06 23:29:33 -0500

    refactor: rename hooks directory to git-hooks

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,10 +4,10 @@ module.exports = {
       'commitlint --config config/commitlint.config.js -E HUSKY_GIT_PARAMS'
     ],
     'pre-commit': [
-      'node scripts/hooks/pre-commit.js'
+      'node scripts/git-hooks/pre-commit.js'
     ],
     'pre-push': [
-      'node scripts/hooks/pre-push.js'
+      'node scripts/git-hooks/pre-push.js'
     ],
   }
 };
