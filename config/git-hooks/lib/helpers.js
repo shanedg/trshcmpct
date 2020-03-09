908877da695cd30463f47b4d996a6c023112ec7a
Shane Garrity <shanedgarrity@gmail.com>
2020-03-09 02:45:26 -0400

    refactor: rename err to error

--- config/git-hooks/lib/helpers.js
+++ config/git-hooks/lib/helpers.js
@@ -1,11 +1,11 @@
 /**
  * Propagate exit error code.
- * @param {Error} err
+ * @param {Error} error
  * @param {*} _stdout
  * @param {*} _stderr
  */
-const handleChildExit = (err, _stdout, _stderr) => {
-  if (err) {
+const handleChildExit = (error, _stdout, _stderr) => {
+  if (error) {
     process.exit(-1);
   }
 
