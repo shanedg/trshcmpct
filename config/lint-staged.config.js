955f6af6149215a9b910ce2b777b9a5568d743fe
Shane Garrity <shanedgarrity@gmail.com>
2020-03-09 03:27:17 -0400

    style: put ESLint flags on separate lines

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,7 +1,10 @@
 const createIgnoredFilter = require('./filter-ignored');
 
 const isIgnored = createIgnoredFilter();
-const eslintFlags = '--max-warnings 0 --fix --cache --cache-location node_modules/.cache/eslint-cache/';
+const eslintFlags = '--max-warnings 0 \
+                     --fix \
+                     --cache \
+                     --cache-location node_modules/.cache/eslint-cache/';
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': files =>
