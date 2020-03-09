9d4adb239b901845fce0b5ed19f6c48543cc6214
Shane Garrity <shanedgarrity@gmail.com>
2020-03-08 22:53:38 -0400

    docs: improve hooks descriptions

--- config/git-hooks/lib/onPreCommit.js
+++ config/git-hooks/lib/onPreCommit.js
@@ -2,9 +2,8 @@ const childProcess = require('child_process');
 const handleChildExit = require('./handleChildExit');
 
 /**
- * Lint staged files and reject commit on any errors or warnings.
- * Provide option to opt-out and defer linting to pre-push (via environment
- * variable, NO_PRECOMMIT=1).
+ * Lint staged files and reject commit on errors.
+ * Defer linting to pre-push via `NO_PRECOMMIT=1`.
  */
 function onPreCommit() {
   if (process.env.NO_PRECOMMIT) {
--- config/git-hooks/lib/onPrePush.js
+++ config/git-hooks/lib/onPrePush.js
@@ -2,10 +2,9 @@ const childProcess = require('child_process');
 const handleChildExit = require('./handleChildExit');
 
 /**
- * Lint entire project only if opted out of linting staged files on pre-commit
- * (via environment variable, NO_PRECOMMIT=1).
- * Perform project type checking and run all project test suites. Reject push
- * on any errors or warnings.
+ * Lint project if `NO_PRECOMMIT=1`.
+ * Check types and run tests.
+ * Reject push on errors.
  */
 function onPrePush() {
   if (process.env.NO_PRECOMMIT) {
