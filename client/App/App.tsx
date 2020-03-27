36d9f1574fcb4b5ff6674a047e5e39b500dc7780
Shane Garrity <shanedgarrity@gmail.com>
2020-03-26 22:53:53 -0400

    feat: content

--- client/App/App.tsx
+++ client/App/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>trshcmpctr</h1>;
+  return (
+    <article>
+
+      <header>
+        <h1>trshcmpctr.com</h1>
+      </header>
+
+      <section>
+        <article>
+          <h2>welcome</h2>
+          <p>hey hello hi</p>
+
+          <section>
+            <h3>the highlights</h3>
+            <ul>
+              <li>list</li>
+              <li>item</li>
+            </ul>
+          </section>
+
+        </article>
+      </section>
+
+    </article>
+  );
 };
 
 export default App;
