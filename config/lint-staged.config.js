8e3b08d2240d4c1c74e499f2a3568a8a0021eba6
Shane Garrity <shanedgarrity@gmail.com>
2020-04-12 17:45:40 -0400

    build(scripts): remove whitespace from ESLint task
    Remove whitespace from lint-staged ESLint task.

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,9 +1,10 @@
 const filterIgnored = require('./filter-ignored')();
 
-const eslintFlags = '--max-warnings 0 \
-                     --fix \
-                     --cache \
-                     --cache-location node_modules/.cache/eslint-cache/';
+const eslintFlags = '\
+--max-warnings 0 \
+--fix \
+--cache \
+--cache-location node_modules/.cache/eslint-cache/';
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': files =>
