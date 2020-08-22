7a2b890624e17d7884e8b375fce3b039caad8bf3
Shane Garrity <shanedgarrity@gmail.com>
2020-08-21 23:21:21 -0400

    docs: add helpful link to commit types

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: [
+    /**
+     * Because I can never remember all the types:
+     * https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/README.md
+     */
+    '@commitlint/config-conventional',
+  ],
 };
