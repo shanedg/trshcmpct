fe9a57a03131571b9871cddf0bdcbcf88efa2b5a
Shane Garrity <shanedgarrity@gmail.com>
2020-08-22 00:07:53 -0400

    build: load css files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -37,7 +37,11 @@ module.exports = (env = {}, argv = {}) => {
           options: {
             configFile: path.resolve(__dirname, './babel.config.js'),
           },
-        }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
       ]
     },
 
