90faa7dd82be3afee223ea425460344b386b814e
Shane Garrity <shanedgarrity@gmail.com>
2020-11-26 04:02:45 -0500

    feat: enforce react strict mode

--- client/App/App.tsx
+++ client/App/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 
 const App = () => {
-  return <h1>trshcmpctr</h1>;
+  return (
+    <StrictMode>
+      <h1>trshcmpctr</h1>
+    </StrictMode>
+  );
 };
 
 export default App;
