c97d5cd829152bd2dca56b25e1e2a6c1878e8eb1
Shane Garrity <shanedgarrity@gmail.com>
2022-02-14 01:45:25 -0500

    build(eslint-ignored): extend local eslint-config

--- eslint-ignored/.eslintrc.js
+++ eslint-ignored/.eslintrc.js
@@ -1,40 +1,7 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
+  extends: ['@trshcmpctr/eslint-config'],
   root: true,
-  rules: {
-    indent: [
-      'warn',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    quotes: [
-      'warn',
-      'single'
-    ],
-    semi: [
-      'warn',
-      'always'
-    ],
-    // Warn on unused variables except underscore-prefixed arguments.
-    'no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^_'
-      }
-    ],
-  },
-  
+
   overrides: [
     // Tests
     {
