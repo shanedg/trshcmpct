2099127a253d2d8d81bb9526f84e5fe528144cc1
Shane Garrity <shanedgarrity@gmail.com>
2022-03-05 23:39:01 -0500

    feat(eslint-config-jest): quote with backticks
    Avoid quotes warnings for Jest inline snapshots that span only 1 line.

--- eslint-config-jest/lib/eslint-config-jest.js
+++ eslint-config-jest/lib/eslint-config-jest.js
@@ -12,5 +12,11 @@ module.exports = {
   // Only include rules that will make sense in both Node and browser environments.
   rules: {
     'jest/consistent-test-it': ['error'],
+    quotes: ['warn', 'single',  {
+      // Jest inline snapshots use backticks so snapshots that span only 1 line generate warnings.
+      allowTemplateLiterals: true,
+      // Allow double quotes if they avoid escaping single quotes.
+      avoidEscape: true,
+    }],
   },
 };
