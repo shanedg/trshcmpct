b094bf816968c5bf2fbdfe286ce371c151fc91de
Shane Garrity <shanedgarrity@gmail.com>
2022-03-29 22:06:36 -0400

    refactor(discord): extract auth fetch

--- discord/lib/index.js
+++ discord/lib/index.js
@@ -6,6 +6,29 @@ const fetch = require('node-fetch');
 
 const { clientId, clientSecret, port } = require('./config.json');
 
+/**
+ * Get auth token
+ * @param {*} code Code from Discord auth redirect
+ * @returns Oauth result
+ */
+const authFromCode = async code => {
+  const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: `http://localhost:${port}`,
+      scope: 'identify',
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+  return await oauthResult.json();
+};
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -21,35 +44,24 @@ app.get('/', async (request, response) => {
   const { code } = request.query;
   if (code) {
     try {
-      const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
-        method: 'POST',
+      // Auth user
+      const oauthData = await authFromCode(code);
 
-        body: new URLSearchParams({
-          client_id: clientId,
-          client_secret: clientSecret,
-          code,
-          grant_type: 'authorization_code',
-          redirect_uri: `http://localhost:${port}`,
-          scope: 'identify',
-        }),
-
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-
-      const oauthData = await oauthResult.json();
+      // Get user details
       const userResult = await fetch('https://discord.com/api/users/@me', {
         headers: {
           authorization: `${oauthData.token_type} ${oauthData.access_token}`,
         },
       });
-
       const { avatar: resultAvatar, discriminator: resultDiscriminator, id: resultId, username: resultUsername } = await userResult.json();
+
       avatar = resultAvatar;
       discriminator = resultDiscriminator;
       id = resultId;
       username = resultUsername;
+
+      // TODO: get user guilds and see if ours is in the list?
+      // TODO: get user has a specific role in our guild?
 
     } catch (error) {
       // NOTE: An unauthorized token will not throw an error;
