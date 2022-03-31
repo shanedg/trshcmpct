6b7f2eeddf6fc33db31a243b8b37f85bfdbac886
Shane Garrity <shanedgarrity@gmail.com>
2022-03-31 00:05:31 -0400

    feat(discord): check guild membership
    Also extracts utils for getting an auth token, currying authorization
    header with token to fetch options, and searching a guild list for a
    specific guild id.

--- discord/lib/index.js
+++ discord/lib/index.js
@@ -1,33 +1,9 @@
 const path = require('path');
 
 const express = require('express');
-// We have to use node-fetch@^2 because node-fetch@>=3 is esm-only.
-const fetch = require('node-fetch');
 
 const { clientId, clientSecret, guildId, port } = require('./config.json');
-
-/**
- * Get auth token
- * @param {*} code Code from Discord auth redirect
- * @returns Oauth result
- */
-const authFromCode = async code => {
-  const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
-    method: 'POST',
-    body: new URLSearchParams({
-      client_id: clientId,
-      client_secret: clientSecret,
-      code,
-      grant_type: 'authorization_code',
-      redirect_uri: `http://localhost:${port}`,
-      scope: 'identify',
-    }),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-  return await oauthResult.json();
-};
+const { authFromCode, getFetchWithOauth, getGuildById } = require('./utils');
 
 const app = express();
 
@@ -45,23 +21,22 @@ app.get('/', async (request, response) => {
   if (code) {
     try {
       // Auth user
-      const oauthData = await authFromCode(code);
+      const oauthResult = await authFromCode({ code, clientId, clientSecret, port });
+      const fetchWithOauth = getFetchWithOauth(await oauthResult.json());
 
       // Get user details
-      const userResult = await fetch('https://discord.com/api/users/@me', {
-        headers: {
-          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-        },
-      });
-      const { avatar: resultAvatar, discriminator: resultDiscriminator, id: resultId, username: resultUsername } = await userResult.json();
+      const userResult = await fetchWithOauth('https://discord.com/api/users/@me');
+      ({ avatar, discriminator, id, username } = await userResult.json());
 
-      avatar = resultAvatar;
-      discriminator = resultDiscriminator;
-      id = resultId;
-      username = resultUsername;
+      // Get user guilds
+      const guildsResult = await fetchWithOauth('https://discord.com/api/users/@me/guilds');
+      const guilds = await guildsResult.json();
 
-      // TODO: get user guilds and see if ours is in the list?
-      // TODO: get user has a specific role in our guild?
+      const guild = getGuildById(guilds, guildId);
+      if (guild) {
+        console.log('in the guild!');
+        // TODO: get user has a specific role in our guild?
+      }
 
     } catch (error) {
       // NOTE: An unauthorized token will not throw an error;
