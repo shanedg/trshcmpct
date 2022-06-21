5c09b4c94ec8f5e30d131abbcf7db450050ee545
Shane Garrity <shanedgarrity@gmail.com>
2022-06-20 22:51:50 -0400

    test(client): replace custom transformer
    Turns out we don't need an entire custom transformer to pass the
    location of the babel config to babel-jest, we can pass it as an option
    from jest transform.

--- client/config/jest/jest.config.js
+++ client/config/jest/jest.config.js
@@ -11,7 +11,12 @@ module.exports = {
     // To workaround this, we configure Drone to remove src/ from the workspace base path.
     // But we also include the root directory in the transform pattern here (/client/src)
     // as an extra defense against transforming unintended files.
-    '\\/client\\/src\\/.+\\.[t|j]sx?$': '<rootDir>/config/jest/babel-jest.js',
+    '\\/client\\/src\\/.+\\.[t|j]sx?$': [
+      'babel-jest',
+      // Since the babel config isn't in the package root,
+      // we need to locate it explicitly for babel-jest here.
+      { configFile: require.resolve('../babel.config.js') }
+    ],
   },
   collectCoverage: true,
   testEnvironment: 'jsdom',
