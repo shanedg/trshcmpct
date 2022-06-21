3a05818a6c580c09d88a84fe603dfa71e3859730
Shane Garrity <shanedgarrity@gmail.com>
2022-06-20 20:22:31 -0400

    test(client): include root dir in transform

--- client/config/jest/jest.config.js
+++ client/config/jest/jest.config.js
@@ -5,8 +5,13 @@ module.exports = {
     '<rootDir>/src',
   ],
   transform: {
-    // Only files in src/ are built & transpiled by Babel.
-    '\\/src\\/.+\\.[t|j]sx?$': '<rootDir>/config/jest/babel-jest.js',
+    // Only files in src/ are built & transpiled by Babel, so only these should be transformed.
+    // Note that the transform pattern is matched against the full path.
+    // This creates some trouble in CI because Drone's default workspace already includes src/ (/drone/src).
+    // To workaround this, we configure Drone to remove src/ from the workspace base path.
+    // But we also include the root directory in the transform pattern here (/client/src)
+    // as an extra defense against transforming unintended files.
+    '\\/client\\/src\\/.+\\.[t|j]sx?$': '<rootDir>/config/jest/babel-jest.js',
   },
   collectCoverage: true,
   testEnvironment: 'jsdom',
