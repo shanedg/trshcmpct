28b4adeaee3b8549f1a165dc5022f61925276020
Shane Garrity <shanedgarrity@gmail.com>
2022-06-27 00:35:58 -0400

    fix(discord): remove whitespace on login link

--- discord/cypress/e2e/authenticate.cy.js
+++ discord/cypress/e2e/authenticate.cy.js
@@ -4,7 +4,6 @@ describe('no auth', () => {
   it('displays login link', () => {
     cy.visit('http://localhost:53134');
     cy.get('#login')
-      // FIXME: html element formatting in index.html
-      .should('have.text', '\n      sign in with discord\n    ');
+      .should('have.text', 'sign in with discord');
   });
 });
