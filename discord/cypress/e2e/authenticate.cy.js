bed52ddc90cb5116bdde4dfa09e15fbb4a19a5b5
Shane Garrity <shanedgarrity@gmail.com>
2022-07-16 16:30:07 -0400

    test(discord): assert against redirect uri

--- discord/cypress/e2e/authenticate.cy.js
+++ discord/cypress/e2e/authenticate.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 
-describe('no auth', () => {
-  it('displays login link', () => {
+describe('login', () => {
+  it('displays login link if not authenticated', () => {
+    const uriEncodedLocalhost = encodeURIComponent('http://localhost:53134');
+    const redirectUriMatcher = new RegExp(`redirect_uri=${uriEncodedLocalhost}`);
+
     cy.visit('/');
     cy.get('#login')
-      .should('have.text', 'sign in with discord');
+      .should('have.text', 'sign in with discord')
+      .invoke('attr', 'href')
+      .should('match', redirectUriMatcher);
   });
 });
