fa34ebadf549ada032c378d5e53ac1b2ad0ef55a
Shane Garrity <shanedgarrity@gmail.com>
2022-07-18 01:28:21 -0400

    test(client): add cypress e2e

---
+++ client/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    setupNodeEvents(_on, _config) {
+      // implement node event listeners here
+    },
+  },
+  video: false,
+});
