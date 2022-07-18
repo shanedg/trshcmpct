1edb63ec24f8cec888524d4cba7426c306c9d7db
Shane Garrity <shanedgarrity@gmail.com>
2022-07-18 01:44:08 -0400

    test(client): ignore spurious error

--- client/src/App/App.test.tsx
+++ client/src/App/App.test.tsx
@@ -13,6 +13,10 @@ describe('App', () => {
 
     await screen.findByRole('heading');
 
+    /* eslint-disable @typescript-eslint/no-unsafe-call */
+    // @ts-expect-error: Property 'toHaveTextContent' does not exist on type 'Assertion'.ts(2339)
+    // FIXME: 'toHaveTextContent' does exist but started throwing after adding Cypress to this project.
     expect(screen.getByRole('heading')).toHaveTextContent('trshcmpctr');
+    /* eslint-enable @typescript-eslint/no-unsafe-call */
   });
 });
