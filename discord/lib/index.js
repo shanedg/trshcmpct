0b7f1c3bb8550d84434d0209ee3e3d35910a7b8c
Shane Garrity <shanedgarrity@gmail.com>
2022-07-23 15:47:14 -0400

    build(discord): use experimental fetch api

--- discord/lib/index.js
+++ discord/lib/index.js
@@ -3,8 +3,6 @@ import { fileURLToPath } from 'node:url';
 
 import cookieSession from 'cookie-session';
 import express from 'express';
-// Someday soon we'll use the fetch api from node itself :)
-import fetch from 'node-fetch';
 import handlebars from 'hbs';
 import pinoHttp from 'pino-http';
 
