92e2336321e480ad4eda7d9175c0c1b65a0a2f6e
Shane Garrity <shanedgarrity@gmail.com>
2022-07-24 03:39:52 -0400

    build: command for vscode eslint extension dirs

--- common/scripts/get_vscode_eslint_extension_working_directories.sh
+++ common/scripts/get_vscode_eslint_extension_working_directories.sh
@@ -5,6 +5,9 @@
 # Providing these working directories ensures that ESLint problem highlighting
 # in the editor respects .eslintrc.* and .eslintignore.
 
+# This script depends on jq: https://stedolan.github.io/jq/
+# On Mac OS X, use Homebrew: `brew install jq`
+
 set -eou pipefail
 
 rush list --json | jq '.projects | { "eslint.workingDirectories": map(.path) }'
