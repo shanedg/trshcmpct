18e91f6e7f0028b4a5f11c684fc6a5c493cb7cef
Shane Garrity <shanedgarrity@gmail.com>
2022-08-21 15:52:15 -0400

    docs(client): question re: previous assumptions

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,9 +2,10 @@ import { dirname, resolve } from 'node:path';
 
 import App from './App/App';
 
-
 // TODO: referencing this file (at least via this mechanism?) creates a [hash].css file in esm/ dir, want to avoid this
 // const pathToStyleSheet = new URL('./style.css', import.meta.url);
+// TODO: actually, do we want to avoid this?
+// not so sure anymore
 const pathToStyleSheet = resolve(dirname(import.meta.url), 'style.css');
 
 const containerId = 'root';
