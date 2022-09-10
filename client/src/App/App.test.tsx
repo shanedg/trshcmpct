4ac51054faec585483c65e44b08c45747762c793
Shane Garrity <shanedgarrity@gmail.com>
2022-09-10 18:55:41 -0400

    docs(client): eslint disable directive comments

--- client/src/App/App.test.tsx
+++ client/src/App/App.test.tsx
@@ -12,10 +12,10 @@ describe('App', () => {
 
     await screen.findByRole('heading');
 
-    /* eslint-disable @typescript-eslint/no-unsafe-call */
+    /* eslint-disable @typescript-eslint/no-unsafe-call -- screen.getByRole "Unsafe call of an `any` typed value" */
     // @ts-expect-error: Property 'toHaveTextContent' does not exist on type 'Assertion'.ts(2339)
     // FIXME: 'toHaveTextContent' does exist but started throwing after adding Cypress to this project.
     expect(screen.getByRole('heading')).toHaveTextContent('trshcmpctr');
-    /* eslint-enable @typescript-eslint/no-unsafe-call */
+    /* eslint-enable @typescript-eslint/no-unsafe-call -- screen.getByRole "Unsafe call of an `any` typed value" */
   });
 });
