a4d13a5824798d9c5e6db7f5571dcc4e0098a5e9
Shane Garrity <shanedgarrity@gmail.com>
2022-11-15 00:46:46 -0500

    feat(client): add nav and welcome to app

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,9 +1,26 @@
 import React, { StrictMode } from 'react';
 
+import { Nav } from './Nav';
+import { Welcome } from './Welcome';
+
 const App = () => {
   return (
     <StrictMode>
       <h1>trshcmpctr</h1>
+      <Welcome />
+      <Nav links={[
+        {
+          href: '#one',
+          label: 'one'
+        },
+        {
+          href: '#two',
+        },
+        {
+          href: '#three',
+          label: 'three'
+        },
+      ]} />
     </StrictMode>
   );
 };
