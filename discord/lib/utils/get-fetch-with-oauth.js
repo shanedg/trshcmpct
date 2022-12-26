0bf1e5f52f5ffc632f69b821f55be58aa330ada2
Shane Garrity <shanedgarrity@gmail.com>
2022-12-25 19:34:13 -0500

    docs(discord): improve fetch helper description

--- discord/lib/utils/get-fetch-with-oauth.js
+++ discord/lib/utils/get-fetch-with-oauth.js
@@ -1,11 +1,11 @@
 /**
  * Curry fetch with authorization header
  * @param {Function} fetch Fetch implementation
- * @param {*} oauthData Oauth result
+ * @param {Object} oauthData Oauth result containing the access token
+ * @param {string} oauthData.access_token Authorization access token
+ * @param {string} oauthData.token_type Type of token
  * @returns Function wrapping fetch with authorization header
  */
-const getFetchWithOauth = (fetch, { token_type: type, access_token: token }) => {
+export const getFetchWithOauth = (fetch, { token_type: type, access_token: token }) => {
   return async url => await fetch(url, { headers: { authorization: `${type} ${token}` }});
 };
-
-export { getFetchWithOauth };
