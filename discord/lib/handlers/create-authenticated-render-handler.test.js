a7b1618bc5acb90f6a898e77dd9927bcf296bc33
Shane Garrity <shanedgarrity@gmail.com>
2023-01-08 22:34:37 -0500

    test(discord): avoid asserting log methods called
    Avoid test file before hook for only one test

--- discord/lib/handlers/create-authenticated-render-handler.test.js
+++ discord/lib/handlers/create-authenticated-render-handler.test.js
@@ -3,21 +3,15 @@ import sinon from 'sinon';
 
 import { createAuthenticatedRenderHandler } from './create-authenticated-render-handler';
 
-const debugSpy = sinon.spy();
-const errorSpy = sinon.spy();
-const nextSpy = sinon.spy();
-const renderSpy = sinon.spy();
-const sendSpy = sinon.spy();
-const sendFileSpy = sinon.spy(() => Promise.resolve());
-const responseWithSpies = { render: renderSpy, send: sendSpy, sendFile: sendFileSpy };
-
-test.before(async () => {
+test('creates a handler that renders the authenticated view', async t => {
+  const sendFileSpy = sinon.spy(() => Promise.resolve());
+  const response = { sendFile: sendFileSpy };
   const reuseSessionToken = createAuthenticatedRenderHandler({
     htmlDirectory: '/absolute/path/to/html/directory',
     htmlFilename: 'index.html',
   });
   const sessionTokenRequest = {
-    log: { error: errorSpy, debug: debugSpy },
+    log: { error: sinon.spy(), debug: sinon.spy() },
     session: {
       oauth: {
         access_token: 'some-access-token',
@@ -25,19 +19,9 @@ test.before(async () => {
       },
     },
   };
-  await reuseSessionToken(sessionTokenRequest, responseWithSpies, nextSpy);
-});
-
-test('creates a handler that > logs to debug', t => {
-  t.plan(2);
-  const debugCalls = debugSpy.getCalls();
-  t.is(debugCalls.length, 1);
-  t.is(debugCalls[0].args[0], 'render authenticated client');
-});
-
-test('creates a handler that > renders the authenticated view', t => {
-  t.plan(3);
+  await reuseSessionToken(sessionTokenRequest, response, sinon.spy());
   const sendFileCalls = sendFileSpy.getCalls();
+  t.plan(3);
   t.is(sendFileCalls.length, 1);
   t.is(sendFileCalls[0].args[0], 'index.html');
   t.is(sendFileCalls[0].args[1].root, '/absolute/path/to/html/directory');
