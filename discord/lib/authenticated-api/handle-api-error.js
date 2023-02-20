d93e22f3edf929af81d4c9f0674be0be21843add
Shane Garrity <shanedgarrity@gmail.com>
2023-02-20 04:13:18 -0500

    feat(discord): handle api errors after headers sent

--- discord/lib/authenticated-api/handle-api-error.js
+++ discord/lib/authenticated-api/handle-api-error.js
@@ -1,9 +1,18 @@
 /**
- * Handle unexpected endpoint errors
- * @param {Object} request
- * @param {Object} response
+ * Custom error handler for unexpected endpoint exceptions
+ * @param {Error} error
+ * @param {express.Request} request
+ * @param {express.Response} response
+ * @param {express.NextFunction} next
  */
-export const handleApiError = (request, response) => {
-  request.log.error('api error');
+export const handleApiError = (error, request, response, next) => {
+  request.log.error(`api error: ${error}`);
+
+  // must delegate to the default express error handler
+  // when headers have already been sent to the client
+  if (response.headersSent) {
+    return next(error);
+  }
+
   response.sendStatus(500);
 };
