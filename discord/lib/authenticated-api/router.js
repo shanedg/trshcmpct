2ee2640dfe840699685789eb7546e799624ba014
Shane Garrity <shanedgarrity@gmail.com>
2023-02-25 22:29:43 -0500

    feat(discord): rewrite api handler as a class

--- discord/lib/authenticated-api/router.js
+++ discord/lib/authenticated-api/router.js
@@ -1,28 +1,63 @@
 import express from 'express';
 
-import config from '../config.json' assert { type: 'json' };
 import { handleApiError } from './handle-api-error';
 import { handleEndpointNotFound } from './handle-endpoint-not-found';
 import { handleGetGuildMembership } from './handle-get-guild-membership';
 import { requireAuthentication } from './require-authentication';
 
-const { guildId } = config;
-
 /**
  * Dedicated router for authenticated endpoints
  */
-const authenticatedApiRouter = express.Router();
+export class AuthenticatedAPIRouter {
+  /**
+   * Create a new AuthenticatedAPIRouter
+   * @param {Object} configuration Configuration options
+   * @param {Function} fetch
+   * @param {string} guildId
+   */
+  constructor(configuration) {
+    const {
+      fetch,
+      guildId,
+    } = configuration;
 
-// All requests to these resources must be authenticated
-authenticatedApiRouter.use(requireAuthentication);
+    if (!fetch) throw new Error('missing fetch');
+    if (!guildId) throw new Error('missing guild id');
 
-// After authentication, check authorization aka membership in target guild
-authenticatedApiRouter.get('/authorized', [
-  handleGetGuildMembership.bind(null, fetch, guildId),
-  handleApiError,
-]);
+    this.configuration = configuration;
+    Object.freeze(this.configuration);
 
-// Any unhandled requests at this point must be 404s
-authenticatedApiRouter.use(handleEndpointNotFound);
+    this.initializeMiddleware();
+  }
 
-export { authenticatedApiRouter };
+  /**
+   * Initialize the authenticated api middleware
+   */
+  initializeMiddleware() {
+    if (this.hasInitialized) throw new Error('auhenticated api middleware already initialized');
+    this.hasInitialized = true;
+
+    const authenticatedApiRouter = express.Router();
+
+    // All requests to these resources must be authenticated
+    authenticatedApiRouter.use(requireAuthentication);
+
+    // After authentication, check authorization aka membership in target guild
+    authenticatedApiRouter.get('/authorized', [
+      handleGetGuildMembership.bind(null, this.fetch, this.guildId),
+      handleApiError,
+    ]);
+
+    // Any unhandled requests at this point must be 404s
+    authenticatedApiRouter.use(handleEndpointNotFound);
+
+    this.router = authenticatedApiRouter;
+  }
+
+  /**
+   * The authenticated api middleware
+   */
+  get middleware() {
+    return this.router;
+  }
+}
