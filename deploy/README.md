b34dc2ecf4cd93a006ee69c9b797f64fa6e65902
Shane Garrity <shanedgarrity@gmail.com>
2023-08-14 15:31:41 -0400

    ci: complete branch and main deployment

--- deploy/README.md
+++ deploy/README.md
@@ -1,3 +1,75 @@
 # @trshcmpctr/deploy
 
-deploy
+## Deploy to AWS EC2
+
+### AWS Console Resources
+
+These resources are managed in the AWS Console
+
+* KeyPair
+* Elastic IPs
+* IAM User
+* IAM Instance Profile
+* Security Group
+* S3 Bucket
+
+#### Key Pair
+
+Provisioned separately to retain the .pem file
+
+##### Elastic IPs
+
+> Domains are custom resource records managed via <https://domains.google.com/>
+
+* <www.trshcmpctr.com>
+Allocation ID: `eipalloc-022c7934990a883c8`
+* <www-stage.trshcmpctr.com>
+Allocation ID: `eipalloc-0b8f0bb1660204bec`
+
+##### IAM User
+
+Credentials provided to GitHub Actions workflows to support:
+
+* starting up a new instance
+* tearing down the old instance
+* uploading build artifacts
+
+Name: `aws-deploy`
+Permissions Policies:
+
+* `AmazonEC2FullAccess`
+* `AmazonS3FullAccess`
+* `IAMFullAccess` (apparently needed to attach instance profile???)
+
+TODO: Reduce permissions to only what deploys need
+
+##### IAM Instance Profile
+
+Credentials attached to running EC2 instances to support:
+
+* downloading build artifacts
+* starting up new instances
+* tearing down old instances
+
+Name: `aws-ec2-discord-runtime`
+Role: `aws-ec2-discord-runtime`
+Permissions Policies:
+
+* `AmazonEC2FullAccess`
+* `AmazonS3FullAccess`
+
+See <https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html>
+
+TODO: Reduce permissions to only what deploys need
+
+##### Security Groups
+
+Rules for traffic ingress and egress, protocols, and ports
+
+* default, all-traffic-from-vpc, default VPC security group
+* ssh-from-my-ip, only-me, only let me in on :22
+* public-http, public http/s, public http and https
+
+##### S3 Bucket
+
+Name: `trshcmpctr.com`
--- deploy/lib/aws-ec2-deploy.js
+++ deploy/lib/aws-ec2-deploy.js
@@ -2,6 +2,10 @@ import { readFileSync } from 'node:fs';
 import { dirname, join } from 'node:path';
 
 import {
+  AssociateAddressCommand,
+  CreateTagsCommand,
+  DescribeInstancesCommand,
+  DescribeInstanceStatusCommand,
   EC2Client,
   RunInstancesCommand,
   TerminateInstancesCommand,
@@ -13,6 +17,7 @@ const {
   accessKeyId,
   amiImageId = 'ami-08a52ddb321b32a8c',
   ec2InstanceType = 't2.nano',
+  // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/locking-api-versions.html
   ec2ApiVersion = '2016-11-15',
   ec2Region = 'us-east-1',
   keyPairName = 'discord-service',
@@ -20,8 +25,6 @@ const {
 } = config;
 
 const ec2Client = new EC2Client({
-  // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/locking-api-versions.html
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html
   apiVersion: ec2ApiVersion,
   region: ec2Region,
   credentials: {
@@ -30,17 +33,51 @@ const ec2Client = new EC2Client({
   },
 });
 
-const ec2DeployUserData = readFileSync(new URL(join(dirname(import.meta.url), 'ec2-user-data.sh')));
+/**
+ * Associate an elastic ip address with an instance
+ * @param {string} instanceId
+ * @param {string} eipAllocationId
+ */
+export const ec2AssociateIp = async(instanceId, eipAllocationId) => await ec2Client.send(new AssociateAddressCommand({
+  InstanceId: instanceId,
+  AllocationId: eipAllocationId,
+  AllowReassociation: true,
+}));
 
 /**
- * Run a new EC2 instance
+ * Create or update instance tags
+ * Existing key values are overwritten
+ * @param {string} resouceId
+ * @param {Object[]} tags
  */
-export const ec2Run = async () => await ec2Client.send(new RunInstancesCommand({
-  InstanceType: ec2InstanceType,
-  ImageId: amiImageId,
-  MaxCount: 1,
-  MinCount: 1,
-  TagSpecifications: [
+export const ec2CreateTag = async (resouceId, tags) => await ec2Client.send(new CreateTagsCommand({
+  Resources: [resouceId],
+  Tags: tags,
+}));
+
+/**
+ * Get instances that match a set of filters
+ * @param {Object[]} filters
+ */
+export const ec2DescribeByFilters = async (filters) => await ec2Client.send(new DescribeInstancesCommand({
+  Filters: filters,
+}));
+
+/**
+ * Get the state of an instance
+ * @param {string} instanceId
+ */
+export const ec2DescribeStatus = async (instanceId) => await ec2Client.send(new DescribeInstanceStatusCommand({
+  InstanceIds: [instanceId],
+  IncludeAllInstances: true,
+}));
+
+/**
+ * Get instance tags
+ * @param {'staging'|'production'} environment
+ */
+const getNextInstanceTagSpecifications = environment => {
+  return [
     {
       ResourceType: 'instance',
       Tags: [
@@ -50,19 +87,100 @@ export const ec2Run = async () => await ec2Client.send(new RunInstancesCommand({
         },
         {
           Key: 'environment',
-          Value: 'production'
+          Value: environment
+        },
+        {
+          Key: 'lifecycle',
+          Value: 'next'
         },
       ]
     },
-  ],
-  UserData: ec2DeployUserData.toString('base64'),
+  ];
+};
+
+/**
+ * Get environment server name for ec2 user data
+ * @param {'staging'|'production'} environment
+ */
+const getUserDataServerName = environment => {
+  if (environment === 'staging') {
+    return 'server_name=www-stage.trshcmpctr.com';
+  }
+
+  if (environment === 'production') {
+    // TODO: also register domain root trshcmpctr.com?
+    // return 'server_name=trshcmpctr.com';
+    return 'server_name=www.trshcmpctr.com';
+  }
+
+  throw new Error(`Unexpected environment: ${environment}`);
+};
+
+/**
+ * Get environment domains for ec2 user data
+ * @param {'staging'|'production'} environment
+ */
+const getUserDataDomains = environment => {
+  if (environment === 'staging') {
+    return 'domains=www-stage.trshcmpctr.com';
+  }
+
+  if (environment === 'production') {
+    // TODO: also register domain root trshcmpctr.com?
+    // return 'domains=trshcmpctr.com,www.trshcmpctr.com';
+    return 'domains=www.trshcmpctr.com';
+  }
+
+  throw new Error(`Unexpected environment: ${environment}`);
+};
+
+/**
+ * Get environment bucket prefix for ec2 user data
+ * @param {'staging'|'production'} environment
+ */
+const getUserDataBucketPrefix = environment => {
+  if (environment === 'staging') {
+    return 'bucket_prefix=www-stage';
+  }
+
+  if (environment === 'production') {
+    return 'bucket_prefix=www';
+  }
+
+  throw new Error(`Unexpected environment: ${environment}`);
+};
+
+/**
+ * Run a new instance
+ * @param {'staging'|'production'} environment
+ */
+export const ec2Run = async environment => await ec2Client.send(new RunInstancesCommand({
+  IamInstanceProfile: {
+    Name: 'aws-ec2-discord-runtime'
+  },
+  InstanceType: ec2InstanceType,
+  ImageId: amiImageId,
+  MaxCount: 1,
+  MinCount: 1,
+  TagSpecifications: getNextInstanceTagSpecifications(environment),
+  UserData: Buffer.from(
+    readFileSync(new URL(join(dirname(import.meta.url), 'ec2-user-data.sh')))
+      .toString()
+      .replace('server_name=www-stage.trshcmpctr.com', getUserDataServerName(environment))
+      .replace('domains=www-stage.trshcmpctr.com', getUserDataDomains(environment))
+      .replace('bucket_prefix=www-stage', getUserDataBucketPrefix(environment))
+  ).toString('base64'),
   KeyName: keyPairName,
+  SecurityGroupIds: [
+    'sg-16d1976a', // default, all-traffic-from-vpc, default VPC security group
+    'sg-06660e98d1262583c', // ssh-from-my-ip, only-me, only let me in on :22
+    'sg-01b47ac15fe770bcb', // public-http, public http/s, public http and https
+  ],
 }));
 
 /**
  * Terminate an EC2 instance
  * @param {string} instanceId
- * @returns
  */
 export const ec2Terminate = async (instanceId) => await ec2Client.send(new TerminateInstancesCommand({
   InstanceIds: [instanceId],
